e448e8c1e2da368650120fa7069ae5e6e3be6e6c
Miguelangel Leon Mayuare <mike.mayuare@gmail.com>
2024-09-01 20:21:43 +0200

    Added data validation

--- main.py
+++ main.py
@@ -1,13 +1,28 @@
-from textSummarizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from textSummarizer.pipeline.stage_01_data_ingestion import (
+    DataIngestionTrainingPipeline,
+)
+from textSummarizer.pipeline.stage_02_data_validation import (
+    DataValidationTrainingPipeline,
+)
 from textSummarizer.logging import logger
 
 
-STAGE_NAME = "Data Ingestion Stage"
+stage_name = "Data Ingestion Stage"
 try:
-    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    logger.info(f">>>>>> stage {stage_name} started <<<<<<")
     data_ingestion = DataIngestionTrainingPipeline()
     data_ingestion.main()
-    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx=========x")
+    logger.info(f">>>>>> stage {stage_name} completed <<<<<<\n\nx=========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+stage_name = "Data Validation stage"
+try:
+    logger.info(f">>>>>> stage {stage_name} started <<<<<<")
+    data_validation = DataValidationTrainingPipeline()
+    data_validation.main()
+    logger.info(f">>>>>> stage {stage_name} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
