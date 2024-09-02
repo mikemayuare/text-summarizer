9877039ac9dd3add8bc464c126885913c193652e
Miguelangel Leon Mayuare <mike.mayuare@gmail.com>
2024-09-02 17:22:14 +0200

    Added data transformation step

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from textSummarizer.pipeline.stage_01_data_ingestion import (
 from textSummarizer.pipeline.stage_02_data_validation import (
     DataValidationTrainingPipeline,
 )
+from textSummarizer.pipeline.stage_03_data_transformation import (
+    DataTransformationTrainingPipeline,
+)
 from textSummarizer.logging import logger
 
 
@@ -22,6 +25,16 @@ try:
     logger.info(f">>>>>> stage {stage_name} started <<<<<<")
     data_validation = DataValidationTrainingPipeline()
     data_validation.main()
+    logger.info(f">>>>>> stage {stage_name} completed <<<<<<\n\nx==========x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+stage_name = "Data Transformation stage"
+try:
+    logger.info(f">>>>>> stage {stage_name} started <<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
     logger.info(f">>>>>> stage {stage_name} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
